525c8cca27cd22609c465ddec33e36f59b7d3d6f
Erick Oda Coulter <erickodac@hotmail.com>
2026-05-20 09:13:47 -0300

    test: added tests for CreateUserOutput

--- auth_service/src/application/outputs/create_user.rs
+++ auth_service/src/application/outputs/create_user.rs
@@ -19,3 +19,37 @@ impl From<UserEntity> for CreateUserOutput {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::domain::value_objects::{
+        email::Email, hashed_password::HashedPassword, role::Role, username::Username,
+    };
+
+    use super::*;
+
+    #[test]
+    pub fn test_from_user_entity_for_create_user_output() {
+        let username = Username::try_from("Username".to_string()).unwrap();
+        let email = Email::try_from("user@email.com".to_string()).unwrap();
+        let password: HashedPassword = HashedPassword::from_hash("hashed_password".to_string());
+        let role: Role = Role::Supervisor;
+
+        let user_entity: UserEntity = UserEntity::new(username, email, password, role);
+        let id: Uuid = user_entity.get_id();
+
+        let create_user_output: CreateUserOutput = CreateUserOutput::from(user_entity);
+
+        assert_eq!(
+            "Username", create_user_output.username,
+            "Expected username to be 'Username', but found: {}",
+            create_user_output.username
+        );
+        assert_eq!(
+            "user@email.com", create_user_output.email,
+            "Expected email to be user@email.com, but found: {}",
+            create_user_output.email
+        );
+        assert_eq!(id, create_user_output.id);
+    }
+}
