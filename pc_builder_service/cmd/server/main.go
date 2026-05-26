716d409f31b585916217f77849ffbbc3cbc2e6d2
iLorenzz <ldutra0912@gmail.com>
2026-05-26 11:21:15 -0300

    feat: implement simple gRPC server running

--- pc_builder_service/cmd/server/main.go
+++ pc_builder_service/cmd/server/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	pb "github.com/erickoda/build-a-computer/pc_builder_service/pkg/protos"
+	"google.golang.org/grpc"
+)
+
+var addr string = os.Getenv("ADDR")
+
+type Server struct {
+	pb.BuilderServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil{
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterBuilderServiceServer(s, Server{})
+	
+	log.Println("gRPC server running on port 50051...")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
+
