45d0620c53839057dc872539b25aab55a1fa45a3
iLorenzz <ldutra0912@gmail.com>
2026-05-28 19:26:53 -0300

    feat: implement ram memory entity

---
+++ pc_builder_service/internal/domain/entities/ram_memory.go
@@ -0,0 +1,19 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type RamMemory struct {
+	ID uuid.UUID `gorm:"type:uuid not null"`
+	Brand string `gorm:"type:varchar(255) not null"`
+	MemoryAmount int32 `gorm:"type:integer not null"`
+	FrequencyMhz int32 `gorm:"type:integer not null"`
+	Series string `gorm:"type:varchar(255) not null"`
+	AvgPrice float32 `gorm:"type:float not null"`
+	Img []byte `gorm:"type:bytea"`
+	CreatedAt time.Time `gorm:"type:timestamp not null"`
+	UpdatedAt time.Time `gorm:"type:timestamp"`
+}
