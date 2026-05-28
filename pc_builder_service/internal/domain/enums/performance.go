52ba2b1feb19ff8f7be8ac6608e68019dd9e8682
iLorenzz <ldutra0912@gmail.com>
2026-05-28 20:43:38 -0300

    refactor: change performance enum to string structure

--- pc_builder_service/internal/domain/enums/performance.go
+++ pc_builder_service/internal/domain/enums/performance.go
@@ -1,21 +1,32 @@
 package enums
 
-type Performance int32
-
-const (
-	PerformanceLow Performance = iota
-	PerformanceMedium
-	PerformanceHigh
-	PerformanceUltra
+import (
+	"database/sql/driver"
+	"fmt"
 )
 
-var peformance_name = map[Performance]string{
-	PerformanceLow:     "Low",
-	PerformanceMedium:  "Medium",
-	PerformanceHigh:    "High",
-	PerformanceUltra:   "Ultra",
+type Performance string
+
+const (
+	PerformanceLow 		Performance = "low"
+	PerformanceMedium 	Performance = "medium"
+	PerformanceHigh 	Performance = "high"
+	PerformanceUltra 	Performance = "ultra"
+)
+
+func (p *Performance) Scan(value interface{}) error {
+	if value == nil{
+		return fmt.Errorf("can not scan a nil value")
+	}
+
+	sv, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("failed to scan performance: %v", value)
+	}
+	*p = Performance(sv)
+	return nil
 }
 
-func (p Performance) String() string {
-	return peformance_name[p]
+func (p Performance) Value() (driver.Value, error) {
+	return string(p), nil
 }
