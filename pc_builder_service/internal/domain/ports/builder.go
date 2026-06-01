348b63776a621ca03b53eab40f3f2de6af865252
iLorenzz <ldutra0912@gmail.com>
2026-06-01 01:30:48 -0300

    feat: add arror as a return of all service methods

--- pc_builder_service/internal/domain/ports/builder.go
+++ pc_builder_service/internal/domain/ports/builder.go
@@ -1,11 +1,19 @@
 package ports
 
 import (
+	"context"
+
 	model "github.com/erickoda/build-a-computer/pc_builder_service/internal/domain/models"
 )
 
 type BuilderPort interface {
-	GetBenchmarksByHavierGame(games []string) []model.Benchmark
-	GetBenchmarkByBestScore([]model.Benchmark) []model.Benchmark
-	GetMotherBoardBySocketAndPCIEAndDDRAndPrice(socket string, pcie string, ddr string, price float32) []model.MotherBoard
+	GetBenchmarksByHavierGame(ctx context.Context, games []string, resolution int32) ([]model.Benchmark, error)
+	GetBenchmarkByBestScore(ctx context.Context, benchmarks []model.Benchmark) ([]model.Benchmark, error)
+	GetMotherBoardBySocketAndPCIEAndDDRAndPrice(
+		ctx context.Context, 
+		socket string, 
+		pcie string, 
+		ddr string, 
+		price float32,
+	) ([]model.MotherBoard, error)
 }
