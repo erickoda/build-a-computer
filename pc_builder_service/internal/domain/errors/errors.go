744dc80727899fc5b9f79396d3779d0fdfe7002c
iLorenzz <ldutra0912@gmail.com>
2026-06-01 01:02:16 -0300

    feat: implement gorm errors and parser uuid error

---
+++ pc_builder_service/internal/domain/errors/errors.go
@@ -0,0 +1,11 @@
+package domain
+
+import "errors"
+
+var (
+	ErrGameNotFound       = errors.New("none game was found")
+	ErrBenchmarkNotFound  = errors.New("none benchmark was found")
+	ErrInternalDatabaseError = errors.New("internal database error")
+
+	ErrInvalidUUID = errors.New("invalid uuid format")
+)
