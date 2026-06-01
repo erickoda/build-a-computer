2752dd00fcb1219bda454ef5d9734d453c589ab6
iLorenzz <ldutra0912@gmail.com>
2026-06-01 01:03:32 -0300

    feat: implement gorm errors handler

---
+++ pc_builder_service/internal/adapters/db/errors.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/erickoda/build-a-computer/pc_builder_service/internal/domain/errors"
+	"gorm.io/gorm"
+)
+
+func HandleError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		if strings.Contains(err.Error(), "benchmark") {
+			return domain.ErrBenchmarkNotFound
+		}
+		if strings.Contains(err.Error(), "game") {
+			return domain.ErrGameNotFound
+		}
+	}
+
+	return domain.ErrInternalDatabaseError
+}
