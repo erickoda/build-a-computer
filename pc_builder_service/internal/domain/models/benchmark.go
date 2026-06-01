56f5e95d7dcef88c4fdb9f313bf8aab495ba55c4
iLorenzz <ldutra0912@gmail.com>
2026-06-01 15:35:25 -0300

    fix: add the exactly name of computer performance column in benchmark table

--- pc_builder_service/internal/domain/models/benchmark.go
+++ pc_builder_service/internal/domain/models/benchmark.go
@@ -11,7 +11,7 @@ type Benchmark struct {
 	ID uuid.UUID `gorm:"type:uuid;primaryKey"`
 	Title string `gorm:"type:varchar(255);not null"`
 	Resolution int32 `gorm:"type:integer;not null"`
-	ComputerPerformance e.ComputerPerformance `gorm:"type:performance;not null"`
+	ComputerPerformance e.ComputerPerformance `gorm:"column:performance;type:performance;not null"`
 	CPUId uuid.UUID `gorm:"type:uuid;not null"`
 	GPUId uuid.UUID `gorm:"type:uuid"`
 	RAMId uuid.UUID `gorm:"type:uuid;not null"`
