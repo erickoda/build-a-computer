0838ffd14051f84d057422e5b0bc2d344a99959c
iLorenzz <ldutra0912@gmail.com>
2026-06-02 15:40:48 -0300

    fix: add correct parameters to  get benchmark by best score service

--- pc_builder_service/internal/domain/ports/builder.go
+++ pc_builder_service/internal/domain/ports/builder.go
@@ -4,11 +4,18 @@ import (
 	"context"
 
 	model "github.com/erickoda/build-a-computer/pc_builder_service/internal/domain/models"
+	e "github.com/erickoda/build-a-computer/pc_builder_service/internal/domain/enums"
 )
 
 type BuilderPort interface {
 	GetBenchmarksByHavierGame(ctx context.Context, games []string, resolution int32) ([]model.Benchmark, error)
-	GetBenchmarkByBestScore(ctx context.Context, benchmarks []model.Benchmark) ([]model.Benchmark, error)
+	
+	GetBenchmarksByBestScore(
+		ctx context.Context, 
+		benchmarks []model.Benchmark,
+		requestedPerformance e.ComputerPerformance,
+	) ([]model.Benchmark, error)
+	
 	GetMotherBoardBySocketAndPCIEAndDDRAndPrice(
 		ctx context.Context, 
 		socket string, 
