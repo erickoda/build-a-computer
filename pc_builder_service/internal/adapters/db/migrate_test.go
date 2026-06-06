f2e014594c0f124b0229b82bda2c537bdd60d649
iLorenzz <ldutra0912@gmail.com>
2026-06-05 22:43:40 -0300

    feat: implement test to migrate all models to date base

---
+++ pc_builder_service/internal/adapters/db/migrate_test.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/erickoda/build-a-computer/pc_builder_service/internal/domain/models"
+)
+
+func TestRunMigrate(t *testing.T) {
+	db := &DB{}
+	if err := db.New_data_base(); err != nil {
+		t.Fatalf("failed to run migrate: %v", err)
+	}
+
+	defer db.Close()
+
+	err := db.DB.AutoMigrate(
+		&models.CPU{},
+		&models.GPU{},
+		&models.RamMemory{},
+		&models.Game{},
+		&models.Benchmark{},
+		&models.MotherBoard{},
+		&models.PowerSource{},
+		&models.SSD{},
+	)
+	if err != nil {
+		t.Fatalf("failed to migrate: %v", err)
+	}
+
+	if !db.DB.Migrator().HasTable(&models.Benchmark{}) {
+		t.Fatalf("failed to migrate: %v", err)
+	}
+}
