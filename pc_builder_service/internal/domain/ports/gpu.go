ff85d08cb5f4f012acfd2b19196f75e002d52fe3
iLorenzz <ldutra0912@gmail.com>
2026-06-07 11:39:54 -0300

    fix: fix id paramter type

--- pc_builder_service/internal/domain/ports/gpu.go
+++ pc_builder_service/internal/domain/ports/gpu.go
@@ -4,8 +4,9 @@ import (
 	"context"
 
 	"github.com/erickoda/build-a-computer/pc_builder_service/internal/domain/models"
+	"github.com/google/uuid"
 )
 
 type GPURepository interface {
-	FindByID(ctx context.Context, id int32) (*models.GPU, error)
+	FindByID(ctx context.Context, id uuid.UUID) (*models.GPU, error)
 }
--- pc_builder_service/internal/domain/ports/ram_memory.go
+++ pc_builder_service/internal/domain/ports/ram_memory.go
@@ -4,8 +4,9 @@ import (
 	"context"
 
 	"github.com/erickoda/build-a-computer/pc_builder_service/internal/domain/models"
+	"github.com/google/uuid"
 )
 
 type RAMMemory interface {
-	FindByID(ctx context.Context, id int32) (*models.RamMemory, error)
+	FindByID(ctx context.Context, id uuid.UUID) (*models.RamMemory, error)
 }
