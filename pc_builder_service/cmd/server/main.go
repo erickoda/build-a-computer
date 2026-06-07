f9924e9657d533f4e6e0c62b2182476fa1545ffa
iLorenzz <ldutra0912@gmail.com>
2026-06-07 12:13:43 -0300

    feat: insert all depencies to main function and implement recover server method

--- pc_builder_service/cmd/server/main.go
+++ pc_builder_service/cmd/server/main.go
@@ -6,39 +6,78 @@ import (
 	"os"
 
 	pb "github.com/erickoda/build-a-computer/pc_builder_service/pkg/protos"
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 
-	"github.com/erickoda/build-a-computer/pc_builder_service/internal/adapters/db"
+	persist "github.com/erickoda/build-a-computer/pc_builder_service/internal/adapters/db"
+	grpcHandler "github.com/erickoda/build-a-computer/pc_builder_service/internal/adapters/grpc"
+	"github.com/erickoda/build-a-computer/pc_builder_service/internal/services"
 )
 
-var addr string = os.Getenv("ADDR")
+var addr string
 
 type Server struct {
 	pb.BuilderServiceServer
 }
 
+func recoverServer(){
+	if r := recover(); r != nil {
+		log.Println("server recovered from panic:", r)
+	}
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("failed to load .env file: " + err.Error())
+	}
+
+	addr = os.Getenv("ADDR")
+
 	lis, err := net.Listen("tcp", addr)
 	if err != nil{
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	db := &db.DB{}
+	defer recoverServer()
+
+	db := &persist.DB{}
 	if err := db.New_data_base(); err != nil {
 		log.Fatal(err)
 	}
 	
 	log.Println("database running...")
 	defer db.Close()
+	
+	benchmarkRepository := persist.NewBenchmarkRepositoryImpl(db.Get())
+	CPURepository := persist.NewCPURepositoryImpl(db.Get())
+	GPURepository := persist.NewGPURepositoryImpl(db.Get())
+	RAMRepository := persist.NewRAMMemoryRepositoryImpl(db.Get())
+	motherBoardRepository := persist.NewMotherBoardRepositoryImpl(db.Get())
+	powerSourceRepository := persist.NewPowerSourceRepositoryImpl(db.Get())
+	SSDRepository := persist.NewSSDRepositoryImpl(db.Get())
+	gameRepository := persist.NewGameRepositoryImpl(db.Get())
+
+	builderService := services.NewBuilderService(
+		*benchmarkRepository,
+		*CPURepository,
+		*GPURepository,
+		*RAMRepository,
+		*motherBoardRepository,
+		*powerSourceRepository,
+		*SSDRepository,
+		*gameRepository,
+	)
+
+	buidlerHandler := grpcHandler.NewBuilderHandler(builderService)
 
 	s := grpc.NewServer()
 
-	pb.RegisterBuilderServiceServer(s, Server{})
+	pb.RegisterBuilderServiceServer(s, buidlerHandler)
 	
 	log.Println("gRPC server running on port 50051...")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
-	
 }
 
