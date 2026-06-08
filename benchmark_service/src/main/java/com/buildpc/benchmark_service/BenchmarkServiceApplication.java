b59cf87d749517282e06285aa2f943e3deb3f657
dino460 <raphael.z.cali@proton.me>
2026-06-08 11:25:18 -0300

    CHORE :: RESET PROJECT
    Recreate spring project due to broken.

--- benchmark_service/src/main/java/com/buildpc/benchmark_service/BenchmarkServiceApplication.java
+++ benchmark_service/src/main/java/com/buildpc/benchmark_service/BenchmarkServiceApplication.java
@@ -1,13 +1,13 @@
-package com.bac.springboot;
+package com.buildpc.benchmark_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class BenchmarkServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(BenchmarkServiceApplication.class, args);
 	}
 
 }
--- benchmark_service/src/test/java/com/buildpc/benchmark_service/BenchmarkServiceApplicationTests.java
+++ benchmark_service/src/test/java/com/buildpc/benchmark_service/BenchmarkServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bac.springboot;
+package com.buildpc.benchmark_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class BenchmarkServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
