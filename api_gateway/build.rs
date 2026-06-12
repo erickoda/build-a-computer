40ee284e996419f3981b6e5c5154b4c953bd5f02
Erick Oda Coulter <erickodac@hotmail.com>
2026-06-12 13:52:46 -0300

    fix: made the new .proto files more generic

--- api_gateway/build.rs
+++ api_gateway/build.rs
@@ -2,8 +2,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
-            &["../proto/auth/auth.proto", "../proto/user/user.proto"],
-            &["../proto"],
+            &[
+                "../proto/auth/auth.proto",
+                "../proto/user/user.proto",
+                "../proto/recommendation/game.proto",
+                "../proto/recommendation/builder.proto",
+                "../proto/recommendation/benchmark.proto",
+            ],
+            &["../proto", "../proto/recommendation"],
         )?;
 
     Ok(())
