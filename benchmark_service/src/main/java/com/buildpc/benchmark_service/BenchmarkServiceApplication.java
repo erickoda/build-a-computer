93fd876e6c9fc9bb5c61d4391695140c75428e94
iLorenzz <ldutra0912@gmail.com>
2026-06-16 02:56:32 -0300

    feat: enable JPA auditing

--- benchmark_service/src/main/java/com/buildpc/benchmark_service/BenchmarkServiceApplication.java
+++ benchmark_service/src/main/java/com/buildpc/benchmark_service/BenchmarkServiceApplication.java
@@ -2,8 +2,10 @@ package com.buildpc.benchmark_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BenchmarkServiceApplication {
 
 	public static void main(String[] args) {
